1680c3fe463f9161974a6a3286cbe28150450391
Jean-Paul <Jean.paul.bassil@outlook.com>
2023-07-12 16:18:40 +0300

    added the index.html to be served and served it along with the needed styles

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-console.log(app);
 const server = http.createServer(app);
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+  res.sendFile(__dirname + "/index.html");
 });
+
 
 server.listen(3000, () => {
   console.log("Server is listening on port 3000");
